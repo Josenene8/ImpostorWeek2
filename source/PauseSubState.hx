3032e4dbf0cbbb13d43d52d796ad4acd30ddfd22
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-04-02 17:07:48 -0300

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -97,15 +97,7 @@ class PauseSubState extends MusicBeatSubstate
 		}
 
 		changeSelection();
-                
-		#if mobileC
-		addVirtualPad(UP_DOWN, A_B);
-		
-		var camcontrol = new FlxCamera();
-		FlxG.cameras.add(camcontrol);
-		camcontrol.bgColor.alpha = 0;
-		_virtualpad.cameras = [camcontrol];
-		
+
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
 	}
 
