3396c2ba74a5be1c394e5b0b642131b880860c54
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-04-02 21:38:35 -0300

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -1,6 +1,11 @@
 package;
 
-import webm.WebmPlayer;
+import lime.app.Application;
+#if windows
+import Discord.DiscordClient;
+#end
+import openfl.display.BlendMode;
+import openfl.text.TextFormat;
 import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.FlxGame;
@@ -21,10 +26,15 @@ class Main extends Sprite
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
+	public static var watermarks = true; // Whether to put Kade Engine literally anywhere
+
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
 	public static function main():Void
 	{
+
+		// quick checks 
+
 		Lib.current.addChild(new Main());
 	}
 
@@ -41,6 +51,8 @@ class Main extends Sprite
 			addEventListener(Event.ADDED_TO_STAGE, init);
 		}
 	}
+
+	public static var webmHandler:WebmHandler;
 
 	private function init(?E:Event):Void
 	{
@@ -66,43 +78,52 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		#if !debug
-		initialState = TitleState;
+		#if !cpp
+		framerate = 60;
 		#end
-		
-		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 
+		#if cpp
+		initialState = Caching;
+		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+		#else
+		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+		#end
 		addChild(game);
 
+		var ourSource:String = "assets/videos/daWeirdVid/dontDelete.webm";
 
-		var ourSource:String = "assets/videos/DO NOT DELETE OR GAME WILL CRASH/dontDelete.webm";
-        
-        #if web
-        var str1:String = "HTML CRAP";
-        var vHandler = new VideoHandler();
-        vHandler.init1();
-        vHandler.video.name = str1;
-        addChild(vHandler.video);
-        vHandler.init2();
-        GlobalVideo.setVid(vHandler);
-        vHandler.source(ourSource);
-        #elseif desktop
-		WebmPlayer.SKIP_STEP_LIMIT = 90; //haxelib git extension-webm https://github.com/ThatRozebudDude/extension-webm
-        var str1:String = "WEBM SHIT"; 
-        var webmHandle = new WebmHandler();
-        webmHandle.source(ourSource);
-        webmHandle.makePlayer();
-        webmHandle.webm.name = str1;
-        addChild(webmHandle.webm);
-        GlobalVideo.setWebm(webmHandle);
-        #end 
+		#if web
+		var str1:String = "HTML CRAP";
+		var vHandler = new VideoHandler();
+		vHandler.init1();
+		vHandler.video.name = str1;
+		addChild(vHandler.video);
+		vHandler.init2();
+		GlobalVideo.setVid(vHandler);
+		vHandler.source(ourSource);
+		#elseif desktop
+		var str1:String = "WEBM SHIT"; 
+		var webmHandle = new WebmHandler();
+		webmHandle.source(ourSource);
+		webmHandle.makePlayer();
+		webmHandle.webm.name = str1;
+		addChild(webmHandle.webm);
+		GlobalVideo.setWebm(webmHandle);
+		#end
+		
+		#if windows
+		DiscordClient.initialize();
 
+		Application.current.onExit.add (function (exitCode) {
+			DiscordClient.shutdown();
+		 });
+		 
+		#end
 
 		#if !mobile
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
-
 		#end
 	}
 
